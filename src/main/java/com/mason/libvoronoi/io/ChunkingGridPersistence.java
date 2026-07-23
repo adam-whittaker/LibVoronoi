3558fee993d58d3b30e1071895852b8dfa02fc7d
Adam <adam271828@gmail.com>
2026-07-23 23:41:31 +0100

    Added saving, loading, and exporting mechanics.

--- src/main/java/com/mason/libvoronoi/io/ChunkingGridPersistence.java
+++ src/main/java/com/mason/libvoronoi/io/ChunkingGridPersistence.java
@@ -5,19 +5,18 @@ import com.mason.libvoronoi.algorithms.components.ChunkingGrid;
 
 import java.io.*;
 
-public class ChunkingGridReader{
+public class ChunkingGridPersistence{
 
-
-    public void writeToFile(String filePath, ChunkingGrid<?> grid) throws IOException{
+    public static void writeToFile(File file, ChunkingGrid<?> grid) throws IOException{
         try(DataOutputStream out = new DataOutputStream(
-                new BufferedOutputStream(new FileOutputStream(filePath)))){
+                new BufferedOutputStream(new FileOutputStream(file)))){
             grid.writeToDataStream(out);
         }
     }
 
-    public static <T extends CentroidData> ChunkingGrid<T> readFromFile(String filePath, CentroidDataReader<T> centroidReader) throws IOException{
+    public static <T extends CentroidData> ChunkingGrid<T> readFromFile(File file, CentroidDataReader<T> centroidReader) throws IOException{
         try(DataInputStream in = new DataInputStream(
-                new BufferedInputStream(new FileInputStream(filePath)))){
+                new BufferedInputStream(new FileInputStream(file)))){
             return ChunkingGrid.readFromDataStream(in, centroidReader);
         }
     }
